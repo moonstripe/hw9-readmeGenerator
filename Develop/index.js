0815b0c822ce5bcaaf64c04958f3746c99fc70a5
Kojin Glick <kojinglick@gmail.com>
2020-08-31 23:31:28 -0700

    email functionality

--- Develop/index.js
+++ Develop/index.js
@@ -6,29 +6,35 @@ const gitAPI = 'https://api.github.com/users/';
 const user = 'moonstripe11';
 
 //prompt
-inquirer.prompt([
-    {
-        type: 'input',
-        name: 'username',
-        message: 'github user',
-    },
-    {
-        type: 'input',
-        name: 'repository',
-        messages: 'repository',
+// inquirer.prompt([
+//     {
+//         type: 'input',
+//         name: 'username',
+//         message: 'github user',
+//     },
+//     {
+//         type: 'input',
+//         name: 'repository',
+//         messages: 'repository',
+//
+//     },
+// ]).then()
 
-    },
-]).then(answers => {
     //git api
     axios
-        .get(`${gitAPI}${answers.username}/repos`)
+        .get(`${gitAPI}hoffstadt/events`)
         .then(response => {
             //repo info
-            let repObj = response.data.filter(item => item.name === answers.repository ? true : false)[0];
-            console.log(repObj);
+            let email = response.data.filter(item => item.payload.commits ? true : false)
+
+            console.log(email[0]);
+            // email address
+            console.log(email[0].payload.commits[0].author.email);
+
+            // let evObj = response.data.filter(item => item.payload.commits[0].author.email ? true : false)[0];
+            // console.log(evObj);
         })
         .catch(error => console.log(error))
-})
 
 
 
